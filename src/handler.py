e32e65aaafd098bf767ed91e0a5b308afd1b572e
nvagon <matiasagnetii@gmail.com>
2025-01-31 11:18:19 -0600

    Updated Params

--- src/handler.py
+++ src/handler.py
@@ -18,18 +18,29 @@ except RuntimeError:
 
 def handler(job):
     """ Handler function that will be used to process jobs. """
-    job_input = job['input']
-    prompt = job_input['prompt']
+    job_input = job["input"]
+    prompt = job_input["prompt"]
+    width = job_input["width"]
+    height = job_input["height"]
+    steps = job_input["steps"]
+    seed = job_input["seed"]
 
     time_start = time.time()
-    image = pipe(prompt=prompt, num_inference_steps=1, guidance_scale=0.0).images[0]
+    image = pipe(
+        prompt=prompt, 
+        width=width, 
+        height=height, 
+        num_inference_steps=steps,         
+        seed=seed, 
+        guidance_scale=0.0
+    ).images[0]
     print(f"Time taken: {time.time() - time_start}")
 
     buffer = io.BytesIO()
     image.save(buffer, format="PNG")
     image_bytes = buffer.getvalue()
 
-    return base64.b64encode(image_bytes).decode('utf-8')
+    return base64.b64encode(image_bytes).decode("utf-8")
 
 
 runpod.serverless.start({"handler": handler})
